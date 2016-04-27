b6320d96ba2737a9c56662fb217c340468c3d22e
nicole <nicolew@shapeways.com>
2016-04-27 19:55:49 -0400

    adding example for shapejs customization via api

---
+++ php-pecl/ShapeJSCustomization-oauth1-pecl.php
@@ -0,0 +1,50 @@
+#!/usr/bin/php
+<?php
+
+require "consumer_key.php";
+require "access_token.php";
+require "api_url_base.php";
+require "error.php";
+
+try {
+    $oauth = new Oauth($consumer_key, $consumer_secret, OAUTH_SIG_METHOD_HMACSHA1, OAUTH_AUTH_TYPE_AUTHORIZATION);
+    $oauth->enableDebug();
+    $oauth->setToken($access_token, $access_secret);
+} catch(OAuthException $E) {
+    Error("setup exception", $E->getMessage(), null, null, $E->debugInfo);
+}
+
+try {
+    $text = "customText";
+
+    $filename = "customPic.png";
+    $picture = file_get_contents("../pics/". $filename);
+
+    $spin = "ABC123";  // CHANGEME // SPIN for a product that has already been set up as a custom maker product
+    $data = array(
+      "spin" => "$spin",
+      "customParam" => array(
+        "text" => "$text",
+        "picture" => rawurlencode(base64_encode($picture)),
+        "pictureName" => $filename
+      )
+    );
+
+    $data_string = json_encode($data);
+    $oauth->fetch($api_url_base ."/shapejs/v1", $data_string, OAUTH_HTTP_METHOD_POST, array("Accept" => "application/json"));
+    $response = $oauth->getLastResponse();
+    $json = json_decode($response);    
+    if (null == $json) {
+        PrintJsonLastError();
+        var_dump($response);
+    } else {
+        print_r($json);
+    }
+} catch(OAuthException $E) {
+
+  print_r($E);
+
+}
+
+?>
+
